98c8bd6be4755e750330b1a78834eb5bca0eb810
CyberXivig <cyberxivig@CyberXivigs-iMac.local>
2020-04-17 04:53:57 -0700

    Batch commit Script

---
+++ trick/RemoveFileFromRepository.sh
@@ -0,0 +1,13 @@
+# Remove existing files from the repository:
+
+find . -name .DS_Store -print0 | xargs -0 git rm -f --ignore-unmatch
+
+# Add the line .DS_Store to the file .gitignore, which can be found at the top level of your repository (or created if it isn't there already). or 
+
+#You can do this easily with this command in the top directory
+
+echo .DS_Store >> .gitignore
+# Then
+
+git add .gitignore
+git commit -m '.DS_Store banished!'
